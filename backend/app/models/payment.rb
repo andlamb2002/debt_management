06d3f5b70815342efe59be64f663514cec90bd7c
Andreas Lambropoulos <andlamb2002@gmail.com>
2026-03-25 10:54:20 -0400

    Updates status automatically in model

--- backend/app/models/payment.rb
+++ backend/app/models/payment.rb
@@ -3,4 +3,19 @@ class Payment < ApplicationRecord
 
     validates :amount, presence: true
     validates :date_paid, presence: true
+
+    after_save :update_debt_status
+    after_destroy :update_debt_status
+
+    private
+
+    def update_debt_status
+        total_paid = debt.payments.sum(:amount)
+        if total_paid >= debt.principal_amt && debt.status == "active"
+            debt.update_column(:status, "paid")
+        end
+        if total_paid < debt.principal_amt && debt.status == "paid"
+            debt.update_column(:status, "active")
+        end
+    end
 end
