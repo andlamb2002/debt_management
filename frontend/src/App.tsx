9a42009648048d914841009be57e604674e61474
Andreas Lambropoulos <andlamb2002@gmail.com>
2026-03-27 14:47:42 -0400

    Basic MUI setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
+import { Button, Card, Container, Typography } from '@mui/material'
 import './App.css'
 
 function App() {
 
     return (
-        <>
-            Hello World!
-        </>
+        <Container>
+            <Typography variant='h3'>Organization</Typography>
+            <Button>Add</Button>
+            <Card>Name Amt_Left Date Status</Card>
+        </Container>
     )
 }
 
