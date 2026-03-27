7637aab39a8e9a70ef92ade63a14c4e20fcb6605
Andreas Lambropoulos <andlamb2002@gmail.com>
2026-03-27 14:52:34 -0400

    Basic prototype

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
-import { Button, Card, Container, Typography } from '@mui/material'
+import { Box, Button, Card, Container, Typography } from '@mui/material'
 import './App.css'
 
 function App() {
 
     return (
         <Container>
-            <Typography variant='h3'>Organization</Typography>
-            <Button>Add</Button>
-            <Card>Name Amt_Left Date Status</Card>
+            <Box sx={{display: 'flex'}}>
+                <Typography variant='h3'>Organization</Typography>
+                <Button>Add</Button>
+            </Box>
+            <Box>
+                <Card>Name Amt_Left Date Status</Card>
+                <Card>Name Amt_Left Date Status</Card>
+                <Card>Name Amt_Left Date Status</Card>
+            </Box>
         </Container>
     )
 }
